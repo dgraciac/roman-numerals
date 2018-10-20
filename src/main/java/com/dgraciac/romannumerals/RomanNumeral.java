e5315c2e03d05f06e21e1b6fa244a3caf57c92ec
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2018-10-20 20:37:29 +0200

    changes

--- src/main/java/com/dgraciac/romannumerals/RomanNumeral.java
+++ src/main/java/com/dgraciac/romannumerals/RomanNumeral.java
@@ -5,6 +5,10 @@ public class RomanNumeral {
     private static final String ROMAN_FIVE = "V";
     private static final String ROMAN_TEN = "X";
     private static final String ROMAN_FIFTY = "L";
+    private static final String ROMAN_HUNDRED = "C";
+    private static final String ROMAN_FIVE_HUNDRED = "D";
+    private static final String ROMAN_THOUSAND = "M";
+
     private final StringBuilder stringBuilder;
 
     public RomanNumeral() {
@@ -12,24 +16,40 @@ public class RomanNumeral {
     }
 
     public String translateFromDecimal(int decimalNumber) {
-        if (decimalNumber < 11) translateFromDecimalLesserThan11(decimalNumber);
-        else if (decimalNumber < 21) {
-            stringBuilder.append(ROMAN_TEN);
-            translateFromDecimalLesserThan11(decimalNumber - 10);
-        } else if (decimalNumber < 31) {
-            stringBuilder.append(ROMAN_TEN).append(ROMAN_TEN);
-            translateFromDecimalLesserThan11(decimalNumber - 20);
-        } else if (decimalNumber < 41) {
-            stringBuilder.append(ROMAN_TEN);
-            if (decimalNumber < 40) {
-                stringBuilder.append(ROMAN_TEN).append(ROMAN_TEN);
-                translateFromDecimalLesserThan11(decimalNumber - 30);
-            } else stringBuilder.append(ROMAN_FIFTY);
-        } else if(decimalNumber < 50){
-            stringBuilder.append(ROMAN_TEN).append(ROMAN_FIFTY);
-            translateFromDecimalLesserThan11(decimalNumber - 40);
-        } else stringBuilder.append(ROMAN_FIFTY);
+        if (decimalNumber < 1000) translateFromDecimalLesserThan1000(decimalNumber);
+        else if (decimalNumber < 2000) {
+            stringBuilder.append(ROMAN_THOUSAND);
+            if (decimalNumber > 1000) translateFromDecimalLesserThan1000(decimalNumber - 1000);
+        } else if (decimalNumber < 3000) {
+            stringBuilder.append(ROMAN_THOUSAND).append(ROMAN_THOUSAND);
+            if (decimalNumber > 2000) translateFromDecimalLesserThan1000(decimalNumber - 2000);
+        } else if (decimalNumber < 4000) {
+            stringBuilder.append(ROMAN_THOUSAND).append(ROMAN_THOUSAND).append(ROMAN_THOUSAND);
+            if (decimalNumber > 3000) translateFromDecimalLesserThan1000(decimalNumber - 3000);
+        } else {
+            stringBuilder.append(ROMAN_THOUSAND).append(ROMAN_THOUSAND).append(ROMAN_THOUSAND).append(ROMAN_THOUSAND);
+            if (decimalNumber > 4000) translateFromDecimalLesserThan1000(decimalNumber - 4000);
+        }
         return stringBuilder.toString();
+    }
+
+    private void translateFromDecimalLesserThan1000(int decimalNumber) {
+        if (decimalNumber < 900) translateFromDecimalLesserThan900(decimalNumber);
+        else if (decimalNumber < 1000) {
+            stringBuilder.append(ROMAN_HUNDRED).append(ROMAN_THOUSAND);
+            if (decimalNumber > 900) translateFromDecimalLesserThan900(decimalNumber - 900);
+        }
+    }
+
+    private void translateFromDecimalLesserThan900(int decimalNumber) {
+        if (decimalNumber < 400) translateFromDecimalLesserThan400(decimalNumber);
+        else if (decimalNumber < 500) {
+            stringBuilder.append(ROMAN_HUNDRED).append(ROMAN_FIVE_HUNDRED);
+            if (decimalNumber > 400) translateFromDecimalLesserThan100(decimalNumber - 400);
+        } else if (decimalNumber < 900) {
+            stringBuilder.append(ROMAN_FIVE_HUNDRED);
+            if (decimalNumber > 500) translateFromDecimalLesserThan400(decimalNumber - 500);
+        }
     }
 
     private void translateFromDecimalLesserThan11(int decimalNumber) {
@@ -43,6 +63,49 @@ public class RomanNumeral {
         } else if (decimalNumber <= 10) {
             if (decimalNumber == 9) stringBuilder.append(ROMAN_ONE);
             stringBuilder.append(ROMAN_TEN);
+        }
+    }
+
+    private void translateFromDecimalLesserThan40(int decimalNumber) {
+        if (decimalNumber < 11) translateFromDecimalLesserThan11(decimalNumber);
+        else if (decimalNumber < 21) {
+            stringBuilder.append(ROMAN_TEN);
+            translateFromDecimalLesserThan11(decimalNumber - 10);
+        } else if (decimalNumber < 31) {
+            stringBuilder.append(ROMAN_TEN).append(ROMAN_TEN);
+            translateFromDecimalLesserThan11(decimalNumber - 20);
+        } else if (decimalNumber < 40) {
+            stringBuilder.append(ROMAN_TEN).append(ROMAN_TEN).append(ROMAN_TEN);
+            translateFromDecimalLesserThan11(decimalNumber - 30);
+        }
+    }
+
+    private void translateFromDecimalLesserThan100(int decimalNumber) {
+        if (decimalNumber < 40) translateFromDecimalLesserThan40(decimalNumber);
+        else if (decimalNumber < 41) stringBuilder.append(ROMAN_TEN).append(ROMAN_FIFTY);
+        else if (decimalNumber < 50) {
+            stringBuilder.append(ROMAN_TEN).append(ROMAN_FIFTY);
+            translateFromDecimalLesserThan11(decimalNumber - 40);
+        } else if (decimalNumber < 90) {
+            stringBuilder.append(ROMAN_FIFTY);
+            if (decimalNumber > 50) translateFromDecimalLesserThan40(decimalNumber - 50);
+        } else if (decimalNumber < 100) {
+            stringBuilder.append(ROMAN_TEN).append(ROMAN_HUNDRED);
+            if (decimalNumber > 90) translateFromDecimalLesserThan11(decimalNumber - 90);
+        }
+    }
+
+    private void translateFromDecimalLesserThan400(int decimalNumber) {
+        if (decimalNumber < 100) translateFromDecimalLesserThan100(decimalNumber);
+        else if (decimalNumber < 200) {
+            stringBuilder.append(ROMAN_HUNDRED);
+            if (decimalNumber > 100) translateFromDecimalLesserThan100(decimalNumber - 100);
+        } else if (decimalNumber < 300) {
+            stringBuilder.append(ROMAN_HUNDRED).append(ROMAN_HUNDRED);
+            if (decimalNumber > 200) translateFromDecimalLesserThan100(decimalNumber - 200);
+        } else if (decimalNumber < 400) {
+            stringBuilder.append(ROMAN_HUNDRED).append(ROMAN_HUNDRED).append(ROMAN_HUNDRED);
+            if (decimalNumber > 300) translateFromDecimalLesserThan100(decimalNumber - 300);
         }
     }
 }
--- src/test/java/com/dgraciac/romannumerals/RomanNumeralShould.java
+++ src/test/java/com/dgraciac/romannumerals/RomanNumeralShould.java
@@ -17,27 +17,27 @@ public class RomanNumeralShould {
         romanNumeral = new RomanNumeral();
     }
 
-/*                "1000,M","2000,MM","3000,MMM","4000,MMMM",
-                        "500,D","1500,MD","2500,MMD","3500,MMMD","4500,MMMMD",
-                        "100,C","200,CC","300,CCC","400,CD","600,DC","700,DCC","800,DCCC","900,CM",
-                        "1100,MC",
-                        "1200,MCC","1300,MCCC","1400,MCD","1600,MDC","1700,MDCC","1800,MDCCC","2100,MMC","3100,MMMC","4100,MMMMC",
-                        ,"10,X",
-                        ,"20,XX",
-                        "4,IV","40,XL",
-                        "5,V","50,L",
-                        "6,VI","60,LX",
-                        "7,VII","70,LXX",
-                        "8,VIII","80,LXXX",
-                        "9,IX","90,XC"*/
-
     @Test
     @Parameters({
             "1,I", "2,II", "3,III", "4,IV", "5,V", "6,VI", "7,VII", "8,VIII", "9,IX", "10,X",
             "11,XI", "12,XII", "13,XIII", "14,XIV", "15,XV", "16,XVI","17,XVII","18,XVIII","19,XIX","20,XX",
             "21,XXI","23,XXIII","24,XXIV","25,XXV","26,XXVI","28,XXVIII","29,XXIX","30,XXX",
             "31,XXXI","33,XXXIII","34,XXXIV","35,XXXV","36,XXXVI","38,XXXVIII","39,XXXIX","40,XL",
-            "41,XLI","42,XLII","43,XLIII","44,XLIV","45,XLV","46,XLVI","48,XLVIII","49,XLIX","50,L"
+            "41,XLI", "42,XLII", "43,XLIII", "44,XLIV", "45,XLV", "46,XLVI", "47,XLVII", "48,XLVIII", "49,XLIX", "50,L",
+            "51,LI", "53,LIII", "54,LIV", "55,LV", "56,LVI", "58,LVIII", "59,LIX", "60,LX",
+            "61,LXI", "70,LXX",
+            "71,LXXI", "80,LXXX",
+            "81,LXXXI", "89,LXXXIX", "90,XC",
+            "91,XCI", "99,XCIX", "100,C",
+            "101,CI", "139,CXXXIX", "140,CXL", "199,CXCIX", "200,CC",
+            "201,CCI", "299,CCXCIX", "300,CCC",
+            "301,CCCI", "399,CCCXCIX", "400,CD",
+            "401,CDI", "499,CDXCIX", "500,D", "899,DCCCXCIX", "900,CM",
+            "901,CMI", "999,CMXCIX", "1000,M",
+            "1001,MI", "1999,MCMXCIX", "2000,MM",
+            "2001,MMI", "2999,MMCMXCIX", "3000,MMM",
+            "3001,MMMI", "3999,MMMCMXCIX", "4000,MMMM",
+            "4001,MMMMI", "4999,MMMMCMXCIX"
     })
     public void translateProperly(int decimalNumber, String romanNumber) {
         assertThat(romanNumeral.translateFromDecimal(decimalNumber)).isEqualTo(romanNumber);
