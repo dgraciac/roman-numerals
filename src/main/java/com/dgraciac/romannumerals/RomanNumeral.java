e9728641e5d5707ef2f45eb41dc727e5dff2941c
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2018-10-20 19:01:26 +0200

    changes

--- src/main/java/com/dgraciac/romannumerals/RomanNumeral.java
+++ src/main/java/com/dgraciac/romannumerals/RomanNumeral.java
@@ -1,78 +1,48 @@
 package com.dgraciac.romannumerals;
 
 public class RomanNumeral {
+    private static final String ROMAN_ONE = "I";
+    private static final String ROMAN_FIVE = "V";
+    private static final String ROMAN_TEN = "X";
+    private static final String ROMAN_FIFTY = "L";
+    private final StringBuilder stringBuilder;
+
+    public RomanNumeral() {
+        stringBuilder = new StringBuilder();
+    }
+
     public String translateFromDecimal(int decimalNumber) {
-        StringBuilder stringBuilder = new StringBuilder();
-        if (decimalNumber % 1000 == 0) {
-            return translateDecimalDivisibleBy1000(decimalNumber, stringBuilder);
-        }
-
-        if (decimalNumber % 500 == 0) {
-            return translateDecimalDivisibleBy500(decimalNumber, stringBuilder);
-        }
-
-        if(decimalNumber % 100 == 0) {
-            int hundreds = decimalNumber / 100;
-            if(hundreds < 4) {
-                for (int i = 0; i < hundreds; i++) stringBuilder.append("C");
-                return stringBuilder.toString();
-            }
-
-            if(hundreds > 5 && hundreds < 9) {
-                stringBuilder.append('D');
-                for (int i = 5; i < hundreds; i++) stringBuilder.append('C');
-                return stringBuilder.toString();
-            }
-            if (decimalNumber == 400) return "CD";
-            if (decimalNumber == 900) return "CM";
-            if (decimalNumber == 1100) return "MC";
-        }
-
-        if (decimalNumber == 1) return "I";
-        if (decimalNumber == 10) return "X";
-
-
-        if (decimalNumber == 2) return "II";
-        if (decimalNumber == 20) return "XX";
-
-
-        if (decimalNumber == 4) return "IV";
-        if (decimalNumber == 40) return "XL";
-
-        if (decimalNumber == 5) return "V";
-        if (decimalNumber == 50) return "L";
-
-        if (decimalNumber == 6) return "VI";
-        if (decimalNumber == 60) return "LX";
-
-        if (decimalNumber == 7) return "VII";
-        if (decimalNumber == 70) return "LXX";
-
-        if (decimalNumber == 8) return "VIII";
-        if (decimalNumber == 80) return "LXXX";
-
-        if (decimalNumber == 9) return "IX";
-        if (decimalNumber == 90) return "XC";
-
-        return null;
-    }
-
-    private String translateDecimalDivisibleBy500(int decimalNumber, StringBuilder stringBuilder) {
-        int fiveHundreds = decimalNumber / 500;
-        if(fiveHundreds == 1) return "D";
-        return translateDecimalDivisibleBy500GreaterThan500(stringBuilder, fiveHundreds);
-    }
-
-    private String translateDecimalDivisibleBy500GreaterThan500(StringBuilder stringBuilder, int fiveHundreds) {
-        stringBuilder.append('M');
-        for (int i = 3; i < fiveHundreds; i += 2) stringBuilder.append('M');
-        stringBuilder.append('D');
+        if (decimalNumber < 11) translateFromDecimalLesserThan11(decimalNumber);
+        else if (decimalNumber < 21) {
+            stringBuilder.append(ROMAN_TEN);
+            translateFromDecimalLesserThan11(decimalNumber - 10);
+        } else if (decimalNumber < 31) {
+            stringBuilder.append(ROMAN_TEN).append(ROMAN_TEN);
+            translateFromDecimalLesserThan11(decimalNumber - 20);
+        } else if (decimalNumber < 41) {
+            stringBuilder.append(ROMAN_TEN);
+            if (decimalNumber < 40) {
+                stringBuilder.append(ROMAN_TEN).append(ROMAN_TEN);
+                translateFromDecimalLesserThan11(decimalNumber - 30);
+            } else stringBuilder.append(ROMAN_FIFTY);
+        } else if(decimalNumber < 50){
+            stringBuilder.append(ROMAN_TEN).append(ROMAN_FIFTY);
+            translateFromDecimalLesserThan11(decimalNumber - 40);
+        } else stringBuilder.append(ROMAN_FIFTY);
         return stringBuilder.toString();
     }
 
-    private String translateDecimalDivisibleBy1000(int decimalNumber, StringBuilder stringBuilder) {
-        int thousands = decimalNumber / 1000;
-        for (int i = 0; i < thousands; i++) stringBuilder.append('M');
-        return stringBuilder.toString();
+    private void translateFromDecimalLesserThan11(int decimalNumber) {
+        if (decimalNumber <= 4) {
+            stringBuilder.append(ROMAN_ONE);
+            if (decimalNumber < 4) for (int i = 1; i < decimalNumber; i++) stringBuilder.append(ROMAN_ONE);
+            else stringBuilder.append(ROMAN_FIVE);
+        } else if (decimalNumber < 9) {
+            stringBuilder.append(ROMAN_FIVE);
+            if (decimalNumber > 5) for (int i = 0; i < decimalNumber - 5; i++) stringBuilder.append(ROMAN_ONE);
+        } else if (decimalNumber <= 10) {
+            if (decimalNumber == 9) stringBuilder.append(ROMAN_ONE);
+            stringBuilder.append(ROMAN_TEN);
+        }
     }
 }
--- src/test/java/com/dgraciac/romannumerals/RomanNumeralShould.java
+++ src/test/java/com/dgraciac/romannumerals/RomanNumeralShould.java
@@ -17,21 +17,27 @@ public class RomanNumeralShould {
         romanNumeral = new RomanNumeral();
     }
 
+/*                "1000,M","2000,MM","3000,MMM","4000,MMMM",
+                        "500,D","1500,MD","2500,MMD","3500,MMMD","4500,MMMMD",
+                        "100,C","200,CC","300,CCC","400,CD","600,DC","700,DCC","800,DCCC","900,CM",
+                        "1100,MC",
+                        "1200,MCC","1300,MCCC","1400,MCD","1600,MDC","1700,MDCC","1800,MDCCC","2100,MMC","3100,MMMC","4100,MMMMC",
+                        ,"10,X",
+                        ,"20,XX",
+                        "4,IV","40,XL",
+                        "5,V","50,L",
+                        "6,VI","60,LX",
+                        "7,VII","70,LXX",
+                        "8,VIII","80,LXXX",
+                        "9,IX","90,XC"*/
+
     @Test
     @Parameters({
-            "1000,M","2000,MM","3000,MMM","4000,MMMM",
-            "500,D","1500,MD","2500,MMD","3500,MMMD","4500,MMMMD",
-            "100,C","200,CC","300,CCC","400,CD","600,DC","700,DCC","800,DCCC","900,CM",
-            "1100,MC",
-//            "1200,MCC","1300,MCCC","1400,MCD","1600,MDC","1700,MDCC","1800,MDCCC","2100,MMC","3100,MMMC","4100,MMMMC",
-            "1,I","10,X",
-            "2,II","20,XX",
-            "4,IV","40,XL",
-            "5,V","50,L",
-            "6,VI","60,LX",
-            "7,VII","70,LXX",
-            "8,VIII","80,LXXX",
-            "9,IX","90,XC"
+            "1,I", "2,II", "3,III", "4,IV", "5,V", "6,VI", "7,VII", "8,VIII", "9,IX", "10,X",
+            "11,XI", "12,XII", "13,XIII", "14,XIV", "15,XV", "16,XVI","17,XVII","18,XVIII","19,XIX","20,XX",
+            "21,XXI","23,XXIII","24,XXIV","25,XXV","26,XXVI","28,XXVIII","29,XXIX","30,XXX",
+            "31,XXXI","33,XXXIII","34,XXXIV","35,XXXV","36,XXXVI","38,XXXVIII","39,XXXIX","40,XL",
+            "41,XLI","42,XLII","43,XLIII","44,XLIV","45,XLV","46,XLVI","48,XLVIII","49,XLIX","50,L"
     })
     public void translateProperly(int decimalNumber, String romanNumber) {
         assertThat(romanNumeral.translateFromDecimal(decimalNumber)).isEqualTo(romanNumber);
